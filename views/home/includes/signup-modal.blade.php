5f8b67881e06b0277d946e8859fbfdbde465e4b4
vaibhavraj <vaibhavraj.developer@gmail.com>
2016-03-15 21:30:37 +0530

    Added new views

---
+++ views/home/includes/signup-modal.blade.php
@@ -0,0 +1,52 @@
+<div class="modal fade" id="default" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title" id="defaultModalLabel">Register an Account</h4>
+            </div>
+    <div class="container">
+    <div class="col-md-6">
+        <div class="featured-box featured-box-secundary default info-content">
+            <div class="box-content">
+
+                <form action="" id="" type="post">
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-12">
+                                <label>E-mail Address</label>
+                                <input type="text" value="" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <label>Password</label>
+                                <input type="password" value="" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label>Re-enter Password</label>
+                                <input type="password" value="" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <input type="submit" value="Register" class="btn btn-primary pull-right push-bottom" data-loading-text="Loading...">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
